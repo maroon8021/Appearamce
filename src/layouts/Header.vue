c1c1e7684cdfe2c032912a8232095887f6bd5d34
maroon8021 <okihiutat.iutim@gmail.com>
2019-01-02 00:20:18 +0900

    init with storybook

---
+++ src/layouts/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="apm-header" :class="colorClassName">
+    <div class="title">{{title}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ApmHeader extends Vue {
+  @Prop() title!: string;
+  @Prop() color!: string;
+
+  private colorClassName: string = this.color || 'green';
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.apm-header{
+  display: flex;
+
+  & .title{
+    font-size: 1.6rem;
+    color: #333;
+    padding-left: 5%;
+  }
+
+  &.green{
+    background-color: #00897B;
+    & .title{
+      color: #fff;
+    }
+  }
+
+  &.blue{
+    background-color: #0288D1;
+    & .title{
+      color: #fff;
+    }
+  }
+}
+</style>
---
+++ src/stories/index.stories.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+
+import MyButton from '../components/MyButton.vue'
+import ApmHeader from '../layouts/Header.vue'
+
+storiesOf('Button', module)
+  .add('with text', () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">Hello Button</my-button>',
+    methods: { action: action('clicked') }
+  }))
+  .add('with JSX', () => ({
+    components: { MyButton },
+    render() {
+      return <my-button onClick={this.action}>With JSX</my-button>;
+    },
+    methods: { action: linkTo('Button', 'with some emoji') }
+  }))
+  .add('with some emoji', () => ({
+    components: { MyButton },
+    template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+    methods: { action: action('clicked') }
+  }))
+
+
+storiesOf('ApmHeader', module)
+  .add('default', () => ({
+    components: { ApmHeader },
+    template: '<apm-header title="Default"/>'
+  }))
+  .add('with color blue', () => ({
+    components: { ApmHeader },
+    template: '<apm-header title="With Blue" color="blue"/>'
+  }))
